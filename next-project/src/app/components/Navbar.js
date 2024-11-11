a3a8e8a4271518e792fecad173bcc8067642ff20
TyreeGreen-hub <tgreen016@student.butte.edu>
2024-11-11 23:31:45 +0000

    next-project

--- next-project/src/app/components/Navbar.js
+++ next-project/src/app/components/Navbar.js
@@ -1,5 +1,6 @@
 import Button from './Button'
 import Tag from './Tag'
+import { BeakerIcon, BugAntIcon } from '@heroicons/react/24/solid'
 
 const navigationButtons = [
   {
@@ -17,17 +18,19 @@ const navigationButtons = [
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-purple-60 p-6 items-center sticky top-0 shadow-lg">
-      <div className="logo bg-purple-700 rounded-full p-6 hover:scale-105 transition-all cursor-pointer"></div>
+      <div className="h-10 w-10">
+        <BugAntIcon />
+      </div>
       <div className="flex gap-x-4 items-center">
         {navigationButtons.map(({ href, title, isNew }) => (
           <>
-          <Button href={href} classNameProp='flex text-l'>
-          {isNew && <Tag classNames='mr-2 animate-pulse' />}
-            {title}
+            <Button href={href} classNameProp="flex text-l">
+              {isNew && <Tag classNames="mr-2 animate-pulse" />}
+              {title}
             </Button>
           </>
         ))}
-        </div>
+      </div>
     </nav>
   )
 }
